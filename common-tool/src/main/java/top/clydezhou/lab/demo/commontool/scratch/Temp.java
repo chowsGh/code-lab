ce47d9a6c5c18d09d4fe9edb0fc8ae107ea07493
clydeZhou <chows_ne@126.com>
2020-06-11 21:22:55 +0800

    [简要描述] 新增相关 spring 工程 [详细内容] [作者    ] clyde

---
+++ common-tool/src/main/java/top/clydezhou/lab/demo/commontool/scratch/Temp.java
@@ -0,0 +1,13 @@
+package top.clydezhou.lab.demo.commontool.scratch;
+
+/**
+ * The type Temp.
+ *
+ * @Date 2020.06.11 00:17:00
+ */
+public class Temp {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ etc/git/README.txt
@@ -0,0 +1,2 @@
+设置 git 提交模板
+git config --local commit.template PATH_TO_YOUR_COMMIT_TEMPLATE
